bf0bb8574932cf06f5d557ac02d1914088aaa590
Kamil Jeżowski <kamiljezovvski@gmail.com>
2019-07-17 13:11:45 +0200

    Dodanie connection stringa i contextu

---
+++ PartPicker/DAL/PickerContext.cs
@@ -0,0 +1,40 @@
+﻿using PartPicker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace PartPicker.DAL
+{
+    public class PickerContext : DbContext
+    {
+        public PickerContext() : base("PickerCString")
+        {
+
+        }
+
+        public DbSet<Shop> Shop { get; set; }
+        public DbSet<Case> Case { get; set; }
+        public DbSet<Cpu> Cpu { get; set; }
+        public DbSet<Gpu> Gpu { get; set; }
+        public DbSet<Mobo> Mobo { get; set; }
+        public DbSet<Psu> Psu { get; set; }
+        public DbSet<Ram> Ram { get; set; }
+        public DbSet<Storage> Storage { get; set; }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Rate> Rate { get; set; }
+
+        public DbSet<Build> Build { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
