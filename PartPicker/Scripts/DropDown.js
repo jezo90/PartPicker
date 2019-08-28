6bfdcc712944ac65d74070d3c331dfdfa95ccec2
Adam Jarszak <adam1000a1@gmail.com>
2019-08-28 21:16:13 +0200

    drop-button-images

--- PartPicker/Scripts/DropDown.js
+++ PartPicker/Scripts/DropDown.js
@@ -1,4 +1,5 @@
-﻿function dropdownNav() {
+﻿
+function dropdownNav() {
     var x = document.getElementById("top-nav");
     if (x.className === "top-nav") {
         x.className += " responsive";
@@ -9,16 +10,38 @@
 
 function drop() {
     document.getElementById("myDropdown").classList.toggle("show");
+
+
+
+
+
+    var x = document.getElementById("image_down");
+    if (x.className === "image_down") {
+        x.className += " hide";
+    } else {
+        x.className = "image_down";
+    }
+
+    var x = document.getElementById("image_up");
+    if (x.className === "image_up") {
+        x.className += " show";
+    } else {
+        x.className = "image_up";
+    }
+
+
+
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
+
     if (!e.target.matches('.dropbtn')) {
         var myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
+            
         }
+        
     }
 }
-
-
