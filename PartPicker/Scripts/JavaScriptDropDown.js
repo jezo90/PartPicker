6a96371affd78ad2b30301eef1533225bd3c4817
Adam Jarszak <adam1000a1@gmail.com>
2019-12-15 15:30:27 +0100

    SADSADSAD

--- PartPicker/Scripts/JavaScriptDropDown.js
+++ PartPicker/Scripts/JavaScriptDropDown.js
@@ -1,9 +1,8 @@
 ﻿ var dropdown = document.getElementsByClassName("dropdown-btn");
     var i;
-
+/* Dodanie event listenera dla każdego z przycisków filtrowania */
     for (i = 0; i < dropdown.length; i++) {
         dropdown[i].addEventListener("click", function () {
-            this.classList.toggle("active");
             var dropdownContent = this.nextElementSibling;
             if (dropdownContent.style.display === "block") {
                 dropdownContent.style.display = "none";
